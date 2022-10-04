1641522249c2a06463c5a24f1ce99baf5c3aa0fe
c-kasahara <112584218+c-kasahara@users.noreply.github.com>
2022-10-04 14:35:21 -0400

    case 1 commit

--- week_01/case_study_01.R
+++ week_01/case_study_01.R
@@ -1,2 +1,13 @@
+# load the iris data set
 data(iris)
 
+# Read the help file for the function that calculates the mean
+?mean
+
+# Calculate mean of the Petal.length field
+petal_length_mean <- mean(iris$Petal.Length)
+
+# Plot the distribution of the Petal.length
+hist(iris$Petal.Length, main="Mean Iris Petal Length", xlab="Petal Length", ylim=c(0,40), col="Sky Blue")
+
+dev.off()
